8a457d7cb7080db2688517928b86d090028fb238
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-10-11 15:15:04 -0300

    create timer initial page.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 AOS.init();
+
+const dataEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteEvento % diasEmMs) / horasEmMs);
+    const minutoAteOEvento = Math.floor((distanciaAteEvento % horasEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutoAteOEvento}m ${segundosAteOEvento}s`;
+
+    
+}, 1000);
